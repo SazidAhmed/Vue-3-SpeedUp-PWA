676f64b317f25b26125f29afd003dcc48993b90a
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-28 15:29:06 +0600

    Speed Up

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <p style="color:green">Reaction time - {{ score }} ms</p>
+  <h3 style="color:tomato">Your Rank</h3>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 225) {
+      this.rank = 'INSANE'
+    } else if (this.score < 300) {
+      this.rank = 'SHARP'
+    }else if (this.score < 400) {
+      this.rank = 'GOOD'
+    }else {
+      this.rank = 'Please Concentrate'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.5em;
+    color: purple;
+    font-weight: bold;
+  }
+</style>
